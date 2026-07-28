2669d4ea2d1f2354082f6e1a6e5e657ab407a843
Moyya Sai Sahasra <sahasrasai1817@gmail.com>
2026-07-28 10:45:02 +0000

    css

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to NRIIT Learning Management System")
+let heading=document.getElementById("welcome")
+heading.innerHTML="Welcome future software engineers"
+console.log("Heading element:",heading)
---
+++ js/test.js
@@ -0,0 +1,39 @@
+console.log("Hello world")
+let student_name="Sahasra"
+console.log("Student name is:"+student_name)
+let student_age=20
+console.log("Student age is:"+ student_age)
+let course_name="Python Full Stack"
+console.log("Course name is:"+course_name)
+let fee=10000
+let discount=10
+let discounted_fee=fee-(fee*discount/100)
+console.log("Discounted Fee is:"+ discounted_fee)
+
+let age=18
+if(age>=18){
+    console.log("Eligible for admission")
+}
+else{
+    console.log("Not eligible or admission")
+}
+
+
+for(let i=1; i<=5; i++){
+    console.log("Iteration number:"+ i)
+}
+
+
+let student={
+    name:"Sahasra",
+    age:20,
+    course:"python full stack",
+    fee:10000
+}
+console.log("Student Details:",student)
+
+
+function greetStudent(name){
+    console.log("Hello" +" "+ name +" " +"Welcome to NRIIT Learing Management System")
+}
+greetStudent(student_name)
