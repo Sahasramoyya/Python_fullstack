8f0106a074faead58205a6b441f9c5bcc001fc27
Moyya Sai Sahasra <sahasrasai1817@gmail.com>
2026-07-29 10:47:44 +0000

    js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,33 @@ alert("Welcome to NRIIT Learning Management System")
 let heading=document.getElementById("welcome")
 heading.innerHTML="Welcome future software engineers"
 console.log("Heading element:",heading)
+let msg=document.getElementById("message")
+msg.innerHTML="Javascript is fun"
+console.log("Message element:",msg)
+function showmessage(){
+    alert("Welcome to NRIIT Learning Management System")
+}
+function changeHeading(){
+    document.getElementById("welcome").
+    innerHTML="Welcome Python Fullstack Developers"
+}
+let heading1=document.querySelector("#welcome");console.log("Heading element:",heading1)
+let button=document.getElementById("btnGreeting");
+button.addEventListener("click",function(){
+    alert("Welcome to javascript Event handling")
+});
+let registerForm=document.getElementById("registerForm");
+registerForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    if (!name||!email||!password){
+        alert("please fill in all fields.");
+        return;
+    }
+    alert("Registered Succesful!");
+    console.log("Name:",name);
+    console.log("Email:",email);
+    console.log("Password:",password);
+});
